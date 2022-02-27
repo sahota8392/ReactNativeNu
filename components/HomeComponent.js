1edd5e0743089802ff027d9cf21e474c7451821a
sahota8392 <sahota8392@gmail.com>
2022-02-26 21:04:59 -0800

    Navigation Part 3

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
 
+function RenderItem({item}) {
+    if(item) {                      //if item exists, retrun title of item and image along with item description, if not, then return empty view
+        return(
+            <Card
+                featuredTitle={item.name}
+                image = {require('./images/react-lake.jpg')}>
+                <Text style = {{margin: 10}}>
+                    {item.description}
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
 class Home extends Component {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            campsites: CAMPSITES,
+            promotions: PROMOTIONS,
+            partners: PARTNERS
+        };
+    }
+    
     static navigationOptions = {
         title: 'Home'
     }
 
     render() {
-        return(
-            <View>
-                <Text>Home Component</Text>
-            </View>
+        return(         //Loads child components at once while FlatList uses Lazy loading, render part at a time on screen only to improve performance 
+            <ScrollView>        
+                <RenderItem 
+                    item = {this.state.campsites.filter(campsite => campsite.featured)[0]}/>
+                <RenderItem 
+                    item = {this.state.promotions.filter(promotion => promotion.featured)[0]}/>
+                <RenderItem 
+                    item = {this.state.partners.filter(partner => partner.featured)[0]}/>
+            </ScrollView>
         );
     }
 }
