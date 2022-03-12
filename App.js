c8be4f63db964124e2394a2e8684980495880e84
sahota8392 <sahota8392@gmail.com>
2022-03-11 21:45:31 -0800

    Persist; keeping data saved

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';                   
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 
-const store = ConfigureStore();
+
+const {persistor, store } = ConfigureStore();
 
 export default function App() {       //the provider store passing to store componenet gives Main componenet and it's child componenets ability to connect to redux store
   return (
-    <Provider store ={store}>     
-      <Main />
-    </Provider>
+    <Provider store={store}>
+      <PersistGate
+          loading={<Loading />}       //keeping the favorite saved when you reload app even
+          persistor={persistor}>
+          <Main />
+      </PersistGate>
+  </Provider>     
   );
 }
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -6,10 +6,18 @@ import { comments } from './comments';
 import { promotions } from './promotions';
 import { partners } from './partners';
 import { favorites } from './favorites';
+import {persistStore, persistCombineReducers } from 'redux-persist';        //expo install redux-persist@5.9.1 first then import 
+import storage from 'redux-persist/es/storage';                             //access to local storage on device
+
+const config = {
+    key: 'root',                            //key is required and storage which set to the storage object we imported (default is local storage)
+    storage,
+    debug: true
+}
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {            //replaced combineReducers with the persist  -- handles updaing to local storage
             campsites,
             comments,
             partners,
@@ -19,5 +27,7 @@ export const ConfigureStore = () => {
         applyMiddleware(thunk, logger)
     );
 
-    return store;
-}
+    const persistor = persistStore(store);          //enables store to be persisted and we use this in App.js
+
+    return { persistor, store };
+};
