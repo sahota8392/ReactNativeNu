a6f2b7a16e0ee043bd5cac1b03088838beb167a0
sahota8392 <sahota8392@gmail.com>
2022-03-16 21:25:09 -0700

    Email Sharing Button

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,8 +1,9 @@
 //No state data so only component in this project that does not need to be connected to redux
 import React, { Component } from 'react';
 import { View, Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';      //after installation of expo install react-native-animatable
+import * as MailComposer from 'expo-mail-composer';         //after expo install expo-mail-composer - we are importing all as MailComposer
 
 class Contact extends Component {
     constructor(props) {
@@ -11,6 +12,14 @@ class Contact extends Component {
 
     static navigationOptions = {
         title: 'Contact Us'
+    }
+
+    sendMail() {                                    //for sharing via email - defaul set below when sharing email
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],    //array of emails and the recipient it will email to
+            subject: 'Inquiry',
+            body: 'To Whom It May Concern:'
+        })
     }
 
     render() {
@@ -28,6 +37,19 @@ class Contact extends Component {
                                 Phone: 1-206-555-1234 {"\n"}
                                 Email: campsites@nucamp.co
                             </Text>
+
+                            <Button 
+                                title = 'Send Email'
+                                buttonStyle = {{backgroundColor: '#5637DD', margin: 40}}
+                                icon = {<Icon
+                                            name = 'envelope-o'
+                                            type = 'font-awesome'
+                                            color = '#fff'
+                                            iconStyle={{marginRight: 10}}
+                                        />
+                                }
+                                onPress = {() => this.sendMail()}
+                            />
                     </Card>
                 </Animatable.View>
             </ScrollView>
