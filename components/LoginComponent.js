aec1d554f4e60b5dc29f9626602bd9ffdeaaa461
sahota8392 <sahota8392@gmail.com>
2022-03-19 11:10:15 -0700

    Image Gallery Permission

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -187,6 +187,21 @@ class RegisterTab extends Component {
         this.setState({imageUrl: processedImage.uri});
     }
 
+    getImagesFromGallery = async() => {
+        const cameraRollPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermissions.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
+    }
+
     handleRegister() {                                                                 //EventHandler will be called when the button is pressed
         console.log(JSON.stringify(this.state));
         if (this.state.remember) {                                                  //if remember me checkbox is checked, save the username/pw
@@ -209,9 +224,14 @@ class RegisterTab extends Component {
                             loadingIndicatorSource={require('./images/logo.png')}
                             style={styles.image}
                         />
+
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImagesFromGallery}
                         />
                     </View>
 
